a15dad34358739871b2ad7774e83472fba18a313
Preet <preet16vaishnav@gmail.com>
2024-04-22 23:00:24 +0530

    multivariable error

--- myapp/views.py
+++ myapp/views.py
@@ -1,6 +1,18 @@
-from django.shortcuts import render
-from .models import Food
+from django.shortcuts import render,get_object_or_404
+from .models import Food,Consume
 # Create your views here.
 def index(request):
-    foods = Food.objects.all()
+    
+    if request.method == "POST":
+        food_consumed = request.POST.get('food_consumed', None)
+        consume = get_object_or_404(Food, name=food_consumed)
+        user = request.user
+        consume = Consume(user=user,food_consumed=consume)
+        consume.save()
+        foods = Food.objects.all()
+
+    else:
+        foods = Food.objects.all()
+
+
     return render(request,'myapp/index.html',{'foods':foods})
